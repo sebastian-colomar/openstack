bd7cdc7210a500b3851599b67150f6235ffc1224
academiaonline <73463321+academiaonline@users.noreply.github.com>
2020-12-30 08:21:20 +0100

    Update aws-master-ubuntu.sh

--- bin/aws-master-ubuntu.sh
+++ bin/aws-master-ubuntu.sh
@@ -6,11 +6,13 @@
 #########################################################################
 set +x 	&& test "$debug" = true	&& set -x				;
 #########################################################################
+touch /tmp/microstack_install
 snap install microstack --beta --devmode                                ;
 while true								;
 do 									\
   service snap.microstack.libvirtd status | grep running -q && break    ;
   sleep 10								;
 done									;
+touch /tmp/microstack_init
 microstack init --auto --control                                        ;
 #########################################################################
