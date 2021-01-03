e3d50b8ac53e3226d26746ecae303b69b2f23070
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-01-03 23:09:08 +0100

    Create cluster-openstack-init.sh

---
+++ bin/cluster-openstack-init.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -x
+#	./bin/cluster-openstack-init.sh
+#########################################################################
+#      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
+#      SPDX-License-Identifier:  GPL-2.0-only                           #
+#########################################################################
+set +x && test "$debug" = true && set -x				;
+#########################################################################
+test -n "$debug"	|| exit 101					;
+test -n "$stack"	|| exit 102					;
+#########################################################################
+service=snap.microstack.libvirtd                                        ;
+sleep=10								;
+#########################################################################
+targets=" InstanceMaster1 " 						;
+#########################################################################
+service_wait_targets $service $sleep $stack "$targets"			;
+#########################################################################
+command=" test -f /tmp/microstack_init_finished "                       ;
+send_wait_targets "$command" $sleep $stack "$targets"			;
+#########################################################################
+command=" 								\
+  microstack add-compute | tail -1                                      \
+" 									;
+token_worker="								\
+  $(									\
+    send_wait_targets "$command" $sleep $stack "$targets"		\
+  )									\
+"									;
+#########################################################################
+targets=" InstanceWorker1 InstanceWorker2 InstanceWorker3 " 		;
+#########################################################################
+service_wait_targets $service $sleep $stack "$targets"			;
+#########################################################################
+command=" test -f /tmp/microstack_install_finished "                    ;
+send_wait_targets "$command" $sleep $stack "$targets"			;
+#########################################################################
+command=" microstack init --auto --compute --join $token_worker "       ;
+send_wait_targets "$command" $sleep $stack "$targets"			;
+#########################################################################
