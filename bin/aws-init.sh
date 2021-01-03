624b22fc425ce3c6377001a455fef02a2fcb5803
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-01-03 23:52:33 +0100

    Create aws-init.sh

---
+++ bin/aws-init.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -x
+#	./bin/aws-init.sh
+#########################################################################
+#      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
+#      SPDX-License-Identifier:  GPL-2.0-only                           #
+#########################################################################
+set +x && test "$debug" = true && set -x				;
+#########################################################################
+test -n "$A"	                && export A                 || exit 100 ;
+test -n "$branch" && export branch || exit 102                          ;
+test -n "$debug" 		&& export debug	            || exit 100	;
+test -n "$domain" 		&& export domain	    || exit 100	;
+test -n "$HostedZoneName"	&& export HostedZoneName    || exit 100 ;
+test -n "$Identifier"		&& export Identifier        || exit 100 ;
+test -n "$RecordSetName1"	&& export RecordSetName1    || exit 100 ;
+test -n "$stack"                && export stack	            || exit 100	;
+test -n "$template"		&& export template    	    || exit 100 ;
+test -n "$TypeMaster"		&& export TypeMaster 	    || exit 100 ;
+test -n "$TypeWorker"		&& export TypeWorker 	    || exit 100 ;
+#########################################################################
+BranchAWS=$branch                                                       ;
+caps=CAPABILITY_IAM							;
+NodeInstallUrlPath=https://$domain/$A					;
+template_url=https://$s3domain/$branch_docker_aws/$template		;
+#########################################################################
+path=etc/aws								;
+#########################################################################
+aws cloudformation create-stack 					\
+  --capabilities 							\
+    $caps 								\
+  --parameters 								\
+    ParameterKey=BranchAWS,ParameterValue=$BranchAWS                    \
+    ParameterKey=HostedZoneName,ParameterValue=$HostedZoneName		\
+    ParameterKey=InstanceMasterInstanceType,ParameterValue=$TypeMaster	\
+    ParameterKey=InstanceWorkerInstanceType,ParameterValue=$TypeWorker  \
+    ParameterKey=Identifier,ParameterValue=$Identifier			\
+    ParameterKey=NodeInstallUrlPath,ParameterValue=$NodeInstallUrlPath	\
+    ParameterKey=RecordSetName1,ParameterValue=$RecordSetName1		\
+  --stack-name 								\
+    $stack 								\
+  --template-body                                                       \
+    file://$path/$template                                              \
+  --output 								\
+    text 								\
+									;
+#########################################################################
+while true 								;
+do 									\
+  aws cloudformation describe-stacks 					\
+    --query 								\
+      "Stacks[].StackStatus" 						\
+    --output 								\
+      text 								\
+    --stack-name 							\
+      $stack 								\
+  | 									\
+  grep CREATE_COMPLETE && break 					;
+  sleep 100 								;
+done									;
+#########################################################################
