bc94a59ed86bca9ce0fb07645cdf88638e139577
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-01-04 00:48:03 +0100

    Update init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -7,18 +7,18 @@
 set +x && test "$debug" = true && set -x				;
 #########################################################################
 test -n "$A"	                && export A                 || exit 100 ;
-test -n "$B"	                && export B                 || exit 100 ;
+test -n "$B"	                && export B                 || exit 101 ;
 test -n "$branch" && export branch || exit 102                          ;
-test -n "$debug" 		&& export debug	            || exit 100	;
-test -n "$domain" 		&& export domain	    || exit 100	;
-test -n "$HostedZoneName"	&& export HostedZoneName    || exit 100 ;
-test -n "$Identifier"		&& export Identifier        || exit 100 ;
-test -n "$mode"                 && export mode	            || exit 100	;
-test -n "$RecordSetName1"	&& export RecordSetName1    || exit 100 ;
-test -n "$stack"                && export stack	            || exit 100	;
-test -n "$template"		&& export template    	    || exit 100 ;
-test -n "$TypeMaster"		&& export TypeMaster 	    || exit 100 ;
-test -n "$TypeWorker"		&& export TypeWorker 	    || exit 100 ;
+test -n "$debug" 		&& export debug	            || exit 103	;
+test -n "$domain" 		&& export domain	    || exit 104	;
+test -n "$HostedZoneName"	&& export HostedZoneName    || exit 105 ;
+test -n "$Identifier"		&& export Identifier        || exit 106 ;
+test -n "$mode"                 && export mode	            || exit 107	;
+test -n "$RecordSetName1"	&& export RecordSetName1    || exit 108 ;
+test -n "$stack"                && export stack	            || exit 109	;
+test -n "$template"		&& export template    	    || exit 110 ;
+test -n "$TypeMaster"		&& export TypeMaster 	    || exit 111 ;
+test -n "$TypeWorker"		&& export TypeWorker 	    || exit 112 ;
 #########################################################################
 path=bin								;
 #########################################################################
