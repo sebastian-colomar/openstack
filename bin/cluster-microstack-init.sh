b7ca5f1034728837b0b925730db3a314da70cd4c
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-01-04 00:39:59 +0100

    Update and rename cluster-openstack-init.sh to cluster-microstack-init.sh

--- bin/cluster-microstack-init.sh
+++ bin/cluster-microstack-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-#	./bin/cluster-openstack-init.sh
+#	./bin/cluster-microstack-init.sh
 #########################################################################
 #      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
 #      SPDX-License-Identifier:  GPL-2.0-only                           #
