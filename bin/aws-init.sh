8f11ef328c4e9282359ae2a47df615f589b6020c
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-01-04 00:50:09 +0100

    Update aws-init.sh

--- bin/aws-init.sh
+++ bin/aws-init.sh
@@ -6,17 +6,17 @@
 #########################################################################
 set +x && test "$debug" = true && set -x				;
 #########################################################################
-test -n "$A"	                && export A                 || exit 100 ;
-test -n "$branch" && export branch || exit 102                          ;
-test -n "$debug" 		&& export debug	            || exit 100	;
-test -n "$domain" 		&& export domain	    || exit 100	;
-test -n "$HostedZoneName"	&& export HostedZoneName    || exit 100 ;
-test -n "$Identifier"		&& export Identifier        || exit 100 ;
-test -n "$RecordSetName1"	&& export RecordSetName1    || exit 100 ;
-test -n "$stack"                && export stack	            || exit 100	;
-test -n "$template"		&& export template    	    || exit 100 ;
-test -n "$TypeMaster"		&& export TypeMaster 	    || exit 100 ;
-test -n "$TypeWorker"		&& export TypeWorker 	    || exit 100 ;
+test -n "$A"	                && export A                 || exit 401 ;
+test -n "$branch" && export branch || exit 402                          ;
+test -n "$debug" 		&& export debug	            || exit 403	;
+test -n "$domain" 		&& export domain	    || exit 404	;
+test -n "$HostedZoneName"	&& export HostedZoneName    || exit 405 ;
+test -n "$Identifier"		&& export Identifier        || exit 406 ;
+test -n "$RecordSetName1"	&& export RecordSetName1    || exit 407 ;
+test -n "$stack"                && export stack	            || exit 408	;
+test -n "$template"		&& export template    	    || exit 409 ;
+test -n "$TypeMaster"		&& export TypeMaster 	    || exit 410 ;
+test -n "$TypeWorker"		&& export TypeWorker 	    || exit 411 ;
 #########################################################################
 BranchAWS=$branch                                                       ;
 caps=CAPABILITY_IAM							;
