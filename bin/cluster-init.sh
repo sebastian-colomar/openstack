5d97d9168218c4daab6e8da540f4ad8a1e6f5943
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-01-04 03:08:57 +0100

    Update cluster-init.sh

--- bin/cluster-init.sh
+++ bin/cluster-init.sh
@@ -30,8 +30,8 @@ rm --force --recursive $uuid                                            ;
 #########################################################################
 export -f send_command							;
 export -f send_list_command						;
-export -f send_wait_targets						;
 export -f send_list_targets						;
+export -f send_wait_targets						;
 #########################################################################
 file=cluster-$mode-init.sh						;
 path=bin								;
